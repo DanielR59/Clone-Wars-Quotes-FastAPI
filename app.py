18f0d542e1ae459f646cefc7f176fcb440a17af1
DanielR59 <daniel_pumas_59@hotmail.com>
2021-10-28 19:09:31 -0500

    default español

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, HTTPException, Request, Query
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 import json
 # from typing import Optional
 from random import randint
-from starlette.responses import FileResponse, RedirectResponse
-from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
 
 with open('clonewarsquotes_eng.json','r') as f:
     data = json.load(f)
@@ -16,10 +17,11 @@ app = FastAPI()
 
 @app.get('/')
 def root():
-        return RedirectResponse(url='/en')
+        return RedirectResponse(url='/es')
 
-@app.get('/{lang}')
+@app.get('/{lang}',response_class=HTMLResponse)
 def root(request : Request, lang : str = Query("en",enumerate=["en","es"])):
+    # print(request.url_for('docs'))
     if lang:
         return templates.TemplateResponse("index.html",{"request" : request,"quote": get_random_quote(lang=lang)})
     else:
@@ -35,7 +37,7 @@ def get_all(lang : str = Query("en",enum=["en","es"])):
 @app.get('/random_quote/')
 def get_random_quote(lang : str = Query("en",enum=["en","es"]), just_quote : bool = True):
     if lang == "en":            
-        quote_id = randint(1,128) #Son 132, pero los ultimos capitulos no tienen
+        quote_id = randint(1,129) #Son 132, pero los ultimos capitulos no tienen
         for element in data:
             # print(element)
             if element["id"] == quote_id:
@@ -44,7 +46,7 @@ def get_random_quote(lang : str = Query("en",enum=["en","es"]), just_quote : boo
                 else:
                     return element
     if lang == "es":            
-        quote_id = randint(1,120) #Son 120 falta hacer mejor scrapping
+        quote_id = randint(1,129) 
         for element in data_esp:
             # print(element)
             if element["id"] == quote_id:
@@ -53,6 +55,7 @@ def get_random_quote(lang : str = Query("en",enum=["en","es"]), just_quote : boo
                 else:
                     return element
     raise HTTPException(status_code=404,detail="Quote not found")
+
 @app.get('/quotes/')
 def get_quote_by_chapter_season(season : int = Query(...,gt=0,le=7), chapter:int = Query(...,gt=0,le=22), lang :str = Query("en",enum=["en","es"]), just_quote : bool = True):
     if lang == "en":
